0eff648ccb08773015d1c56ac3713d9d5b24eb18
mayakilbertus <maya.kilbertus@yahoo.de>
2023-11-29 17:15:15 +0100

    connecting cohort to student

--- server/app.js
+++ server/app.js
@@ -4,8 +4,8 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
 
-const Cohort = require("./models/Cohort.model")
-const Student = require("./models/Student.model")
+const Cohort = require("./models/Cohort.model");
+const Student = require("./models/Student.model");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -13,12 +13,12 @@ const Student = require("./models/Student.model")
 
 const cohorts = require("./cohorts.json");
 const students = require("./students.json");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
-  .then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
-  .catch(err => console.error("Error connecting to MongoDB", err));
+  .then((x) => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to MongoDB", err));
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
@@ -45,76 +45,84 @@ app.get("/api/cohorts", (req, res) => {
 });
 
 app.get("/api/cohorts/:cohortId", (req, res) => {
-  const {cohortId} = req.params
+  const { cohortId } = req.params;
 
   Cohort.findById(cohortId)
-    .then( (cohortFromDB) => {
-      res.send(cohortFromDB)
+    .then((cohortFromDB) => {
+      res.send(cohortFromDB);
     })
-    .catch( (error) => {
+    .catch((error) => {
       console.log("Error getting cohort details from DB...", error);
       res.send("Error getting cohort details from DB...");
-    })
-})
+    });
+});
 
 app.post("/api/cohorts", (req, res) => {
   Cohort.create(req.body)
-    .then( () => {
-      res.status(201).send("Cohort created")
+    .then(() => {
+      res.status(201).send("Cohort created");
     })
-    .catch( (error) => {
+    .catch((error) => {
       console.log("Error creating cohort in DB...", error);
       res.send("Error creating cohort in DB...");
-    })
-})
+    });
+});
 
 app.put("/api/cohorts/:cohortId", (req, res) => {
-  const {cohortId} = req.params
+  const { cohortId } = req.params;
 
-  Cohort.findByIdAndUpdate(cohortId, req.body, {new: true})
-  .then( () => {
-    res.send("Cohort is updated")
-  })
-  .catch( (error) => {
-    console.log("Error updating cohort in DB...", error);
-    res.send("Error updating cohort in DB...");
-  })
-})
+  Cohort.findByIdAndUpdate(cohortId, req.body, { new: true })
+    .then(() => {
+      res.send("Cohort is updated");
+    })
+    .catch((error) => {
+      console.log("Error updating cohort in DB...", error);
+      res.send("Error updating cohort in DB...");
+    });
+});
 
 app.delete("/api/cohorts/:cohortId", (req, res) => {
-  const {cohortId} = req.params
+  const { cohortId } = req.params;
 
   Cohort.findByIdAndDelete(cohortId)
-    .then( () => {
-      res.send("Cohort was deleted")
+    .then(() => {
+      res.send("Cohort was deleted");
     })
-    .catch( (error) => {
+    .catch((error) => {
       console.log("Error deleting cohort from DB...", error);
       res.send("Error deleting cohort from DB...");
-    })
-})
+    });
+});
 
 app.get("/api/students", (req, res) => {
-  res.json(students);
+  Student.find()
+    .populate("cohort")
+    .then(() => res.json(students))
+    .catch((error) => {
+      console.log("Error getting students from DB...", error);
+      res.send("Error getting students from DB");
+    });
 });
 
 app.get("/api/students/cohort/:cohortId", (req, res) => {
-  const {cohortId} = req.params
+  const { cohortId } = req.params;
 
-  Student.find({cohort: cohortId})
-    .then( (studentFromDB) => {
-      res.send(studentFromDB)
+  Student.find({ cohort: cohortId })
+    .populate("cohort")
+    .then((studentFromDB) => {
+      res.send(studentFromDB);
     })
-    .catch( (error) => {
+    .catch((error) => {
       console.log("Error getting student details from DB...", error);
       res.send("Error getting student details from DB...");
-    })
-})
+    });
+});
 
 app.get("/api/students/:studentId", (req, res) => {
   const { studentId } = req.params;
 
   Student.findById(studentId)
+    .populate("cohort")
     .then((studentFromDB) => {
       res.send(studentFromDB);
     })
@@ -126,41 +134,40 @@ app.get("/api/students/:studentId", (req, res) => {
 
 app.post("/api/students", (req, res) => {
   Student.create(req.body)
-    .then( (createdStudent) => {
-      res.status(201).send("A student is created")
+    .then((createdStudent) => {
+      res.status(201).send("A student is created");
     })
-    .catch( (error) => {
+    .catch((error) => {
       console.log("Error creating student in DB...", error);
       res.send("Error creating student in DB...");
-    })  
-})
+    });
+});
 
 app.put("/api/students/:studentId", (req, res) => {
-  const {studentId} = req.params
+  const { studentId } = req.params;
 
-  Student.findByIdAndUpdate(studentId, req.body, {new: true})
-    .then( () => {
-      res.send("Student is updated")
+  Student.findByIdAndUpdate(studentId, req.body, { new: true })
+    .then(() => {
+      res.send("Student is updated");
     })
-    .catch( (error) => {
+    .catch((error) => {
       console.log("Error updating student in DB...", error);
       res.send("Error updating student in DB...");
-    })
-})
+    });
+});
 
 app.delete("/api/students/:studentId", (req, res) => {
-  const {studentId} = req.params
+  const { studentId } = req.params;
 
   Student.findByIdAndDelete(studentId)
-    .then( () => {
-      res.send("Student was deleted")
+    .then(() => {
+      res.send("Student was deleted");
     })
-    .catch( (error) => {
+    .catch((error) => {
       console.log("Error deleting student from DB...", error);
       res.send("Error deleting student from DB...");
-
-    })
-})
+    });
+});
 
 // START SERVER
 app.listen(PORT, () => {
